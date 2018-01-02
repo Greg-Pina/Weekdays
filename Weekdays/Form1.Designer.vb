77236393a8213846b912c995ef516e72e8e07d5d
Greg Pina <pinagm2825@gmail.com>
2018-01-02 05:28:31 -0500

    Initial commit

---
+++ Weekdays/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstResult = New System.Windows.Forms.ListBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.lblMaximum = New System.Windows.Forms.Label()
+        Me.lblMinimum = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lstResult
+        '
+        Me.lstResult.FormattingEnabled = True
+        Me.lstResult.ItemHeight = 16
+        Me.lstResult.Location = New System.Drawing.Point(12, 29)
+        Me.lstResult.Name = "lstResult"
+        Me.lstResult.Size = New System.Drawing.Size(214, 180)
+        Me.lstResult.TabIndex = 0
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(57, 256)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 1
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(206, 256)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(75, 23)
+        Me.btnDisplay.TabIndex = 2
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(352, 256)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Location = New System.Drawing.Point(244, 25)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(334, 23)
+        Me.lblTotal.TabIndex = 4
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAverage.Location = New System.Drawing.Point(247, 78)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(331, 23)
+        Me.lblAverage.TabIndex = 5
+        '
+        'lblMaximum
+        '
+        Me.lblMaximum.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMaximum.Location = New System.Drawing.Point(247, 138)
+        Me.lblMaximum.Name = "lblMaximum"
+        Me.lblMaximum.Size = New System.Drawing.Size(331, 23)
+        Me.lblMaximum.TabIndex = 6
+        '
+        'lblMinimum
+        '
+        Me.lblMinimum.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMinimum.Location = New System.Drawing.Point(250, 187)
+        Me.lblMinimum.Name = "lblMinimum"
+        Me.lblMinimum.Size = New System.Drawing.Size(328, 23)
+        Me.lblMinimum.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(617, 310)
+        Me.Controls.Add(Me.lblMinimum)
+        Me.Controls.Add(Me.lblMaximum)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.lstResult)
+        Me.Name = "Form1"
+        Me.Text = "Weekday Classroom Project"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstResult As System.Windows.Forms.ListBox
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents btnDisplay As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents lblAverage As System.Windows.Forms.Label
+    Friend WithEvents lblMaximum As System.Windows.Forms.Label
+    Friend WithEvents lblMinimum As System.Windows.Forms.Label
+
+End Class
---
+++ Weekdays/Form1.vb
@@ -0,0 +1,152 @@
+﻿Option Strict On
+
+Public Class Form1
+
+    Dim dblHours(6) As Double
+    Dim strDay As String
+    Dim intMaximumDay As Integer
+    Dim intMinimumDay As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        Dim strAmount As String
+        Dim strInputDay As String
+
+        Try
+
+            For i = 0 To dblHours.Length - 1
+
+                strInputDay = GetDay(i)
+                strAmount = InputBox("Please enter hours worked on" & strInputDay)
+                If Validation(strAmount, dblHours(i)) = True Then
+                    lstResult.Items.Add("Hours for" & strInputDay & "are" & "" & dblHours(i))
+                Else
+                    i -= 1
+                End If
+            Next
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+    End Sub
+
+    Public Function Validation(ByVal Hours As String, ByRef HoursNumeric As Double) As Boolean
+
+        If IsNumeric(Hours) Then
+            HoursNumeric = CDbl(Hours)
+        Else
+            MessageBox.Show("Please enter numbers only")
+            Return False
+        End If
+
+        Return True
+
+    End Function
+
+    Private Function GetDay(ByVal Day As Integer) As String
+        Select Case Day
+            Case 0
+                strDay = "Monday"
+            Case 1
+                strDay = "Tuesday"
+            Case 2
+                strDay = "Wednesday"
+            Case 3
+                strDay = "Thursday"
+            Case 4
+                strDay = "Friday"
+            Case 5
+                strDay = "Saturday"
+            Case 6
+                strDay = "Sunday"
+        End Select
+
+        Return strDay
+    End Function
+
+    Private Function GetTotal() As Double
+        Dim dblTotal As Double
+
+        For i = 0 To dblHours.Length - 1
+            dblTotal += dblHours(i)
+        Next
+
+
+        Return dblTotal
+    End Function
+
+
+
+    Private Function GetMaximum() As Double
+        Dim dblMaximum As Double
+
+
+
+        Return dblMaximum
+
+
+    End Function
+
+    Private Function GetMinimum() As Double
+        Dim dblMinimum As Double = 100
+
+
+
+        Return dblMinimum
+
+
+    End Function
+
+    Private Function GetAverage() As Double
+        Dim dblAverageTotal As Double
+        Dim dblAverage As Double
+
+        For i = 0 To dblHours.Length - 1
+            dblAverage += dblHours(i)
+        Next
+        dblAverageTotal = dblAverage / dblHours.Length
+
+        Return dblAverageTotal
+
+    End Function
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+
+        Dim dblTotalDisplay As Double
+        Dim dblMaximumDisplay As Double
+        Dim strDayDisplay As String
+        Dim dblMinimumDisplay As Double
+        Dim dblAverageDisplay As Double
+
+        dblTotalDisplay = GetTotal()
+
+        dblMaximumDisplay = GetMaximum()
+
+        strDayDisplay = GetDay(intMinimumDay)
+
+        lblTotal.Text = "The total hours worked are " & dblTotalDisplay
+
+        lblMaximum.Text = "The maximum daily hours was " & dblMaximumDisplay & " on " & strDayDisplay
+
+        dblMinimumDisplay = GetMinimum()
+
+        strDayDisplay = GetDay(intMinimumDay)
+
+        lblMinimum.Text = "The minimum daily hours was " & dblMinimumDisplay & " on " & strDayDisplay
+
+        dblAverageDisplay = GetAverage()
+
+        lblAverage.Text = "The average hours worked is " & dblAverageDisplay.ToString("G3")
+
+
+    End Sub
+
+End Class
+
